17f47344a221be93dc061fc9ad3dd38efa7d1991
Han Lee <lee46@mail.usf.edu>
2023-10-22 00:41:32 -0400

    sdf

--- pages/Graph.py
+++ pages/Graph.py
@@ -9,33 +9,6 @@ print(df)
 print(df.columns)
 #column1 = pd.DataFrame(df, columns=["PlantName"])
 
-dataArr = pd.DataFrame(df).to_numpy()
-#print(data1)
-
-PlantName = []
-StateProvince = []
-ManufacturingRegion = []
-CountryName = []
-Capacity = []
-Usage = []
-PreferredLogistics = []
-
-for i in range(len(dataArr)):
-    PlantName.append(dataArr[i][0])
-    StateProvince.append(dataArr[i][1])
-    ManufacturingRegion.append(dataArr[i][2])
-    CountryName.append(dataArr[i][3])
-    Capacity.append(dataArr[i][4])
-    Usage.append(dataArr[i][5])
-    PreferredLogistics.append(dataArr[i][6])
-
-print(PlantName)
-print(StateProvince)
-print(ManufacturingRegion)
-print(CountryName)
-print(Capacity)
-print(Usage)
-print(PreferredLogistics)
 
 
 
@@ -45,6 +18,6 @@ print(PreferredLogistics)
 
 #hm.get_chart_1()
 #hm.get_chart_2()
-#hm.get_chart_3(df)
+hm.get_chart_3(df)
 #hmp.get_pdchart1()
 #hmp.get_pdchart2()
