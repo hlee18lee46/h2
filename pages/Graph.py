5dfbcd6555204a52a8e1926e99455f9c13e29da5
Han Lee <lee46@mail.usf.edu>
2023-10-21 23:17:57 -0400

    sdf

--- pages/Graph.py
+++ pages/Graph.py
@@ -3,6 +3,11 @@ import HeatmapGenerator as hm
 import HeatmapPandas as hmp
 import pandas as pd
 
+
+df = pd.read_excel(r"Yazaki.xlsx")
+
+print(df)
+
 hm.get_chart_1()
 hm.get_chart_2()
 #hmp.get_pdchart1()
