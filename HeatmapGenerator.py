842ec9381d4a3b47e767838dabd781777b423f7a
Han Lee <lee46@mail.usf.edu>
2023-10-21 23:25:55 -0400

    sdf

--- HeatmapGenerator.py
+++ HeatmapGenerator.py
@@ -24,3 +24,21 @@ def get_chart_2():
         st.plotly_chart(fig, theme="streamlit")
     with tab2:
         st.plotly_chart(fig, theme=None)
+
+
+
+def get_chart_3(myfile):
+    import plotly.express as px
+    data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+    fig = px.imshow(data,
+                    labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                    x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                    y=['Morning', 'Afternoon', 'Evening']
+                   )
+    fig.update_xaxes(side="top")
+
+    tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+    with tab1:
+        st.plotly_chart(fig, theme="streamlit")
+    with tab2:
+        st.plotly_chart(fig, theme=None)
--- pages/Graph.py
+++ pages/Graph.py
@@ -5,10 +5,13 @@ import pandas as pd
 
 
 df = pd.read_excel(r"Yazaki.xlsx")
-
+print("First row" + df[0])
+print("second row" + df[1])
+print("third row" + df[2])
 print(df)
 
 hm.get_chart_1()
 hm.get_chart_2()
+
 #hmp.get_pdchart1()
 #hmp.get_pdchart2()
