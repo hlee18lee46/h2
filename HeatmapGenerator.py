2905d2151a27717b81b514fa1f2b10c70d7fe26e
Han Lee <lee46@mail.usf.edu>
2023-10-21 23:45:29 -0400

    fg

--- HeatmapGenerator.py
+++ HeatmapGenerator.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 def get_chart_1():
     import plotly.express as px
--- pages/Graph.py
+++ pages/Graph.py
@@ -7,7 +7,8 @@ import pandas as pd
 df = pd.read_excel(r"Yazaki.xlsx")
 print(df)
 print(df.columns)
-
+column1 = pd.DataFrame(df, columns=["PlantName"])
+print(column1)
 
 hm.get_chart_1()
 hm.get_chart_2()
