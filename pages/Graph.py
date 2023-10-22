c3d78a0433e851ec3c62a348cc8d265cd85e61a9
Han Lee <lee46@mail.usf.edu>
2023-10-21 23:15:19 -0400

    sdf

--- pages/Graph.py
+++ pages/Graph.py
@@ -3,7 +3,7 @@ import HeatmapGenerator as hm
 import HeatmapPandas as hmp
 import pandas as pd
 
-#hm.get_chart_1()
-#hm.get_chart_2()
+hm.get_chart_1()
+hm.get_chart_2()
 #hmp.get_pdchart1()
 #hmp.get_pdchart2()
